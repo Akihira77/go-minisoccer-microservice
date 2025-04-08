bed5a9e94a4d6d07055019517dc7bb92ffb51147
Akihira77 <tik.dikawahyu123@gmail.com>
2025-04-08 10:37:33 +0700

    feat: add method for reading config file either from json or consul

---
+++ backend/user-service/config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"os"
+	"user-service/common/util"
+
+	"github.com/sirupsen/logrus"
+)
+
+var config AppConfig
+
+type AppConfig struct {
+	Port                  int      `json:"port"`
+	AppName               string   `json:"appName"`
+	AppEnv                string   `json:"appEnv"`
+	SignatureKey          string   `json:"signatureKey"`
+	Database              Database `json:"database"`
+	RateLimiterMaxRequest float64  `json:"rateLimiterMaxRequest"`
+	RateLimiterTimeSecond int      `json:"rateLimiterTimeSecond"`
+	JwtSecretKey          string   `json:"jwtSecretKey"`
+	JwtExpirationTime     int      `json:"jwtExpirationTime"`
+}
+
+type Database struct {
+	Host                  string `json:"host"`
+	Port                  int    `json:"port"`
+	Name                  string `json:"name"`
+	Username              string `json:"username"`
+	Password              string `json:"password"`
+	MaxOpenConnection     int    `json:"maxOpenConnection"`
+	MaxLifetimeConnection int    `json:"maxLifetimeConnection"`
+	MaxIdleConnection     int    `json:"maxIdleConnection"`
+	MaxIdleTime           int    `json:"maxIdleTime"`
+}
+
+func Init() {
+	var config AppConfig
+	err := util.BindFromJSON(config, "config.json", ".")
+	if err != nil {
+		logrus.Infof("failed to bind config: %v", err)
+
+		err = util.BindFromConsulKV(config, os.Getenv("CONSUL_HTTP_URL"), os.Getenv("CONSUL_HTTP_KEY"))
+		if err != nil {
+			panic(err)
+		}
+	}
+}
