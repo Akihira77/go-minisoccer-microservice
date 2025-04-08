2a983282004313d49d55935dc17c132f7daf1812
Akihira77 <tik.dikawahyu123@gmail.com>
2025-04-08 10:02:06 +0700

    feat: add utility func for reading config json file

---
+++ backend/user-service/common/util/util.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func BindFromJSON(dest any, filename, path string) error {
+	v := viper.New()
+
+	v.SetConfigFile("json")
+	v.AddConfigPath(path)
+	v.SetConfigName(filename)
+
+	err := v.ReadInConfig()
+	if err != nil {
+		return err
+	}
+
+	err = v.Unmarshal(&dest)
+	if err != nil {
+		logrus.Errorf("failed to unmarshal: %v", err)
+		return err
+	}
+
+	return nil
+}
