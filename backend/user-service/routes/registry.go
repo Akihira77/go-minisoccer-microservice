ae27409f2b9a609535779c64d2e370308e638219
Akihira77 <tik.dikawahyu123@gmail.com>
2025-04-10 10:20:17 +0700

    feat: setup user service router

---
+++ backend/user-service/routes/registry.go
@@ -0,0 +1,32 @@
+package routes
+
+import (
+	"user-service/controllers"
+	"user-service/routes/user"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Registry struct {
+	controller controllers.IControllerRegistry
+	group      *gin.RouterGroup
+}
+
+type IRouteRegistry interface {
+	Serve()
+}
+
+func NewRouteRegistry(controller controllers.IControllerRegistry, group *gin.RouterGroup) IRouteRegistry {
+	return &Registry{
+		controller: controller,
+		group:      group,
+	}
+}
+
+func (r *Registry) userRoute() user.IUserRoute {
+	return user.NewUserRoute(r.controller)
+}
+
+func (r *Registry) Serve() {
+	r.userRoute().Run()
+}
---
+++ backend/user-service/routes/user/user.go
@@ -0,0 +1,32 @@
+package user
+
+import (
+	"user-service/controllers"
+	"user-service/middlewares"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserRoute struct {
+	controller controllers.IControllerRegistry
+	group      *gin.RouterGroup
+}
+
+type IUserRoute interface {
+	Run()
+}
+
+func NewUserRoute(controller controllers.IControllerRegistry) IUserRoute {
+	return &UserRoute{
+		controller: controller,
+	}
+}
+
+func (ur *UserRoute) Run() {
+	group := ur.group.Group("/auth")
+	group.GET("/user", middlewares.Authenticate(), ur.controller.GetUserController().GetUserLogin)
+	group.GET("/:uuid", middlewares.Authenticate(), ur.controller.GetUserController().GetUserByUUID)
+	group.POST("/login", ur.controller.GetUserController().Login)
+	group.POST("/register", ur.controller.GetUserController().Register)
+	group.PUT("/:uuid", middlewares.Authenticate(), ur.controller.GetUserController().Update)
+}
